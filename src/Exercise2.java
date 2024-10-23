0d5676f1997253ee201bdbd8ab4ed5315afd39dc
Joe Rice <binkbop101@gmail.com>
2024-10-23 16:26:31 +0100

    EDIT: added task 3 and made it prettier

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,20 +1,32 @@
 public class Exercise2 {
-//    public static void main(String[] args) {
-//        int x = 3, y = 5;
-//        if(x >= 0){
-//            if(y < x){
-//                System.out.println("y is less than c");
-//            }
-//        } else {
-//            System.out.println("x is negative");
-//        }
-//    }
-
     public static void main(String[] args) {
+        //Task 1
+        /*
+        int x = 3, y = 5;
+        if (x >= 0){
+            if (y < x){
+                System.out.println("y is less than c");
+            }
+        } else {
+            System.out.println("x is negative");
+        }
+        */
+
+        //Task 2
+        /*
         int x = 7;
         if (x == 0){
             System.out.println("x is zero");
         }
+        */
+
+        //Task 3
+        /*
+        int x = 16, y = 21;
+        if (x % 2 == 0 && y % 2 == 0) {
+            System.out.println("x and y are even numbers");
+        }
+        */
     }
 }
 
