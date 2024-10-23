ce812624dff5e54c344e09df8612f70fc4a6fac4
Joe Rice <binkbop101@gmail.com>
2024-10-23 16:41:09 +0100

    EDIT: completed What's wrong? Pair 2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -28,34 +28,68 @@ public class Exercise2 {
         }
         */
 
-//        int i = 0;
-//
-//        while (i < 10) {
-//            System.out.println("i = " + i);
-//            i++;
-//        }
+        //Task 4
+        /*
+        int i = 0;
 
-//        int i = 0;
-//        int j = 10;
-//
-//        while (i < j) {
-//            System.out.println("i = " + i);
-//            System.out.println("j = " + j);
-//
-//            i++;
-//            j--;
-//        }
+        while (i < 10) {
+            System.out.println("i = " + i);
+            i++;
+        }
+        */
 
-//        int i = 0;
-//        int sum = 0;
-//
-//        while (i < 10) {
-//            sum += i;
-//            System.out.println("Sum is " + sum);
-//
-//            i++;
-//        }
+        //Task 5
+        /*
+        int i = 0;
+        int j = 10;
 
+        while (i < j) {
+            System.out.println("i = " + i);
+            System.out.println("j = " + j);
+
+            i++;
+            j--;
+        }
+        */
+
+        //Task 6
+        /*
+        int i = 0;
+        int sum = 0;
+
+        while (i < 10) {
+            sum += i;
+            System.out.println("Sum is " + sum);
+
+            i++;
+        }*/
+
+        //Task 7
+        /*
+        int x = 10;
+        while (x > 0){
+            System.out.println(x--);
+        }
+        System.out.println("We have lift off!");
+        */
+
+        //Task 8
+        /*
+        int x = 10;
+        while(x > 0){
+            System.out.println("x is " + x);
+            --x;
+        }
+        */
+
+        //Task 9
+        /*
+        int sum = 0;
+        for(int i = 0; i < 10; i++){
+            sum += i;
+            System.out.println("Sum is " + sum);
+        }
+        */
     }
 }
 
