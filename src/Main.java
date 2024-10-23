767e1dea95648b0de8fee18d4e7affe9dd198ac2
Joe Rice <binkbop101@gmail.com>
2024-10-23 15:05:44 +0100

    EDIT: Task 1 added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
-
-        }
+        String firstName = "Joey";
+        String lastName = "Ferrari";
+        String name = firstName + " " + lastName;
+        System.out.println(name);
     }
 }
