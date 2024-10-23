c7b05a391bb1f6042707ac933ef84e86883166fb
NevaDobric <neva.dobric@gmail.com>
2024-10-23 16:20:42 +0100

    EDIT: task 2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,12 +1,19 @@
 public class Exercise2 {
+//    public static void main(String[] args) {
+//        int x = 3, y = 5;
+//        if(x >= 0){
+//            if(y < x){
+//                System.out.println("y is less than c");
+//            }
+//        } else {
+//            System.out.println("x is negative");
+//        }
+//    }
+
     public static void main(String[] args) {
-        int x = 3, y = 5;
-        if(x >= 0){
-            if(y < x){
-                System.out.println("y is less than c");
-            }
-        } else {
-            System.out.println("x is negative");
+        int x = 7;
+        if (x == 0){
+            System.out.println("x is zero");
         }
     }
 }
